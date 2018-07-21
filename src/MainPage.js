f54bdb06ffac27f380437cf9c55b5db58bfae6ff
Szofi <nyafologus@gmail.com>
2018-07-21 23:16:00 +0200

    [Create] MainPage.js

---
+++ src/MainPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+// TODO: extract part of the ternary operator that refers to the MainPage
+
+class MainPage extends React.Component {
+	render() {
+		return ()
+	}
+}
+
+export default MainPage
