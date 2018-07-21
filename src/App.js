b819400799883ddf44916a0411243e64d5f0a919
Szofi <nyafologus@gmail.com>
2018-07-21 23:13:36 +0200

    [Create] SearchPage.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import SearchPage from './SearchPage.js'
 
 class BooksApp extends React.Component {
   state = {
---
+++ src/SearchPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+// TODO: inserting the first part of the ternary operator that refers to the SearchPage
+
+class SearchPage extends React.Component {
+	render() {
+		return ()
+	}
+}
+
+export default SearchPage
